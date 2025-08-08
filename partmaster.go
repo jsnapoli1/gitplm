4e4274f7e10bae3eb78083906718506280fd63c1
jsnapoli1 <50517925+jsnapoli1@users.noreply.github.com>
2025-08-07 22:38:55 -0600

    Expose all part sources

--- partmaster.go
+++ partmaster.go
@@ -33,8 +33,12 @@ func (p partmaster) String() string {
 	return result
 }
 
-// findPart returns part with highest priority
-func (p *partmaster) findPart(pn ipn) (*partmasterLine, error) {
+// findPartSources returns all matching parts for a given IPN sorted by priority.
+// Missing description, footprint, and value fields are populated from the
+// highest priority part so every returned entry has the same core data. This
+// enables callers to access an arbitrary number of sources for a single part
+// number.
+func (p *partmaster) findPartSources(pn ipn) ([]*partmasterLine, error) {
 	found := []*partmasterLine{}
 	for _, l := range *p {
 		if l.IPN == pn {
@@ -42,28 +46,51 @@ func (p *partmaster) findPart(pn ipn) (*partmasterLine, error) {
 		}
 	}
 
-	if len(found) <= 0 {
+	if len(found) == 0 {
 		return nil, fmt.Errorf("Part not found")
 	}
 
 	sort.Sort(byPriority(found))
 
+	base := found[0]
 	if len(found) > 1 {
-		// fill in blank fields with values from other items
+		// fill in blank fields on the highest priority entry
 		for i := 1; i < len(found); i++ {
-			if found[0].Description == "" && found[i].Description != "" {
-				found[0].Description = found[i].Description
+			if base.Description == "" && found[i].Description != "" {
+				base.Description = found[i].Description
 			}
-			if found[0].Footprint == "" && found[i].Footprint != "" {
-				found[0].Footprint = found[i].Footprint
+			if base.Footprint == "" && found[i].Footprint != "" {
+				base.Footprint = found[i].Footprint
 			}
-			if found[0].Value == "" && found[i].Value != "" {
-				found[0].Value = found[i].Value
+			if base.Value == "" && found[i].Value != "" {
+				base.Value = found[i].Value
 			}
 		}
 	}
 
-	return found[0], nil
+	// propagate common fields to all returned parts
+	for _, f := range found {
+		if f.Description == "" {
+			f.Description = base.Description
+		}
+		if f.Footprint == "" {
+			f.Footprint = base.Footprint
+		}
+		if f.Value == "" {
+			f.Value = base.Value
+		}
+	}
+
+	return found, nil
+}
+
+// findPart returns part with highest priority
+func (p *partmaster) findPart(pn ipn) (*partmasterLine, error) {
+	parts, err := p.findPartSources(pn)
+	if err != nil {
+		return nil, err
+	}
+	return parts[0], nil
 }
 
 // type for sorting
